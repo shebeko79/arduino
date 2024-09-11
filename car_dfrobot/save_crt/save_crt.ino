91a40955c4550f1363f6dcc1fc4c3fbfe2ae7b61
shebeko79 <eugene.shebeko@gmail.com>
2024-09-11 08:19:29 +0200

    1. Save certificate into modem 2. Just regular AT terminal

---
+++ car_dfrobot/save_crt/save_crt.ino
@@ -0,0 +1,108 @@
+#include <DFRobot_SIM7000.h>
+
+#define PIN_TX     7
+#define PIN_RX     8
+SoftwareSerial     mySerial(PIN_RX,PIN_TX);
+DFRobot_SIM7000    sim7000(&mySerial);
+
+const char rootCACertificate[] PROGMEM = 
+//godaddy
+
+"-----BEGIN CERTIFICATE-----\r\n"
+"MIIE0DCCA7igAwIBAgIBBzANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx\r\n"
+"EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT\r\n"
+"EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp\r\n"
+"ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTExMDUwMzA3MDAwMFoXDTMxMDUwMzA3\r\n"
+"MDAwMFowgbQxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH\r\n"
+"EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjEtMCsGA1UE\r\n"
+"CxMkaHR0cDovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMTMwMQYDVQQD\r\n"
+"EypHbyBEYWRkeSBTZWN1cmUgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IC0gRzIwggEi\r\n"
+"MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC54MsQ1K92vdSTYuswZLiBCGzD\r\n"
+"BNliF44v/z5lz4/OYuY8UhzaFkVLVat4a2ODYpDOD2lsmcgaFItMzEUz6ojcnqOv\r\n"
+"K/6AYZ15V8TPLvQ/MDxdR/yaFrzDN5ZBUY4RS1T4KL7QjL7wMDge87Am+GZHY23e\r\n"
+"cSZHjzhHU9FGHbTj3ADqRay9vHHZqm8A29vNMDp5T19MR/gd71vCxJ1gO7GyQ5HY\r\n"
+"pDNO6rPWJ0+tJYqlxvTV0KaudAVkV4i1RFXULSo6Pvi4vekyCgKUZMQWOlDxSq7n\r\n"
+"eTOvDCAHf+jfBDnCaQJsY1L6d8EbyHSHyLmTGFBUNUtpTrw700kuH9zB0lL7AgMB\r\n"
+"AAGjggEaMIIBFjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBBjAdBgNV\r\n"
+"HQ4EFgQUQMK9J47MNIMwojPX+2yz8LQsgM4wHwYDVR0jBBgwFoAUOpqFBxBnKLbv\r\n"
+"9r0FQW4gwZTaD94wNAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8v\r\n"
+"b2NzcC5nb2RhZGR5LmNvbS8wNQYDVR0fBC4wLDAqoCigJoYkaHR0cDovL2NybC5n\r\n"
+"b2RhZGR5LmNvbS9nZHJvb3QtZzIuY3JsMEYGA1UdIAQ/MD0wOwYEVR0gADAzMDEG\r\n"
+"CCsGAQUFBwIBFiVodHRwczovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkv\r\n"
+"MA0GCSqGSIb3DQEBCwUAA4IBAQAIfmyTEMg4uJapkEv/oV9PBO9sPpyIBslQj6Zz\r\n"
+"91cxG7685C/b+LrTW+C05+Z5Yg4MotdqY3MxtfWoSKQ7CC2iXZDXtHwlTxFWMMS2\r\n"
+"RJ17LJ3lXubvDGGqv+QqG+6EnriDfcFDzkSnE3ANkR/0yBOtg2DZ2HKocyQetawi\r\n"
+"DsoXiWJYRBuriSUBAA/NxBti21G00w9RKpv0vHP8ds42pM3Z2Czqrpv1KrKQ0U11\r\n"
+"GIo/ikGQI31bS/6kA1ibRrLDYGCD+H1QQc7CoZDDu+8CL9IVVO5EFdkKrqeKM+2x\r\n"
+"LXY2JtwE65/3YR8V3Idv7kaWKK2hJn0KCacuBKONvPi8BDAB\r\n"
+"-----END CERTIFICATE-----\r\n";
+
+
+
+void readModem()
+{
+  delay(1000);
+  mySerial.listen();
+  while(mySerial.available()){
+    Serial.write(mySerial.read());
+  }
+}
+
+void sendModem(String str)
+{
+  mySerial.print(str);
+  mySerial.print("\r\n");
+  Serial.println(str);
+}
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(19200);
+  sim7000.turnON();
+
+  while(1)
+  {
+    if(sim7000.setBaudRate(19200))
+    {
+        break;
+    }
+    else
+    {
+      delay(1000);
+    }
+  }
+  mySerial.begin(19200);
+  Serial.println("Ready");
+/*
+  sendModem("AT+CFSINIT");
+  readModem();
+  
+  int len = strlen_P(rootCACertificate);
+
+  sendModem("AT+CFSWFILE=3,godaddy.crt,0,"+String(len)+",10000");
+  readModem();
+
+  for (int i = 0; i < len; i++)
+  {
+    char c = pgm_read_byte_near(rootCACertificate + i);
+    mySerial.write(c);
+    Serial.write(c);
+  }
+
+  readModem();
+  sendModem("AT+CFSTERM");
+  readModem();
+  
+}
+*/
+void loop()
+{
+  mySerial.listen();
+  while(mySerial.available()){
+    Serial.write(mySerial.read());
+  }
+  mySerial.flush();
+  while(Serial.available()){
+    mySerial.write(Serial.read());
+  }
+}
